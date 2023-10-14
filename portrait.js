a8bbc4dca940e3921fbc092165c9a30e28b89635
Satvik-P <95187247+Satvik-P@users.noreply.github.com>
2023-10-13 18:54:20 -0700

    Add files via upload

---
+++ portrait.js
@@ -0,0 +1,30 @@
+Status = "";
+portrait_image = "";
+
+function preload(){
+    portrait_image = loadImage("IMG_0692.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(portrait_image,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(portrait_image,0,0,640,350);
+}
